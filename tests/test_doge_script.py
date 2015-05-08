deb1e4e3b2a9774744598aa1ab941ed3cf72bb57
kirillsulim <kirillsulim@gmail.com>
2015-05-08 15:53:11 +0500

    Changes in arguments processing

--- tests/test_doge_script.py
+++ tests/test_doge_script.py
@@ -4,9 +4,15 @@ from doge_script import process_arguments as parse
 
 
 class ArgsParsing(unittest.TestCase):
+    """\
+    Test argument parsing function
+
+    NB: 'doge' in array is for beauty reason (mimics call from shell). First argument contains script path and always be
+    ignored.
+    """
     def test_no_arg(self):
         res = parse([
-            'Path_to_exec_never_checked',
+            'doge',
         ])
         self.assertEqual(res, {
             'no': True,
@@ -14,10 +20,39 @@ class ArgsParsing(unittest.TestCase):
 
     def test_init(self):
         res = parse([
-            'Path_to_exec_never_checked',
+            'doge',
             'init',
         ])
-        #self.assertEqual(res, {
-        #    'no': True,
-        #})
+        self.assertEqual(res, {
+            'init': True,
+            'use_venv': True,
+        })
+
+    def test_init_no_venv(self):
+        res = parse([
+            'doge',
+            'init',
+            '--no-venv',
+        ])
+        self.assertEqual(res, {
+            'init': True,
+        })
+
+    def test_unknown(self):
+        res = parse([
+            'doge',
+            'some_nonexisting_command',
+        ])
+        self.assertEquals(res, {
+            'unknown': True
+        })
+
+    def test_help(self):
+        res  = parse([
+            'doge',
+            'help',
+        ])
+        self.assertEquals(res, {
+            'help': True,
+        })
 
