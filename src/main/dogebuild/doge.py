b6fa79ede4dadc69004e7869575b01676eaadd1a
Kirill Sulim <kirillsulim@gmail.com>
2018-01-04 14:17:05 +0500

    kwargs added to use_plugin function

--- src/main/dogebuild/doge.py
+++ src/main/dogebuild/doge.py
@@ -1,5 +1,5 @@
 import imp
-from adapters.pip import PipAdapter
+from dogebuild.adapters.pip import PipAdapter
 
 
 class Doge:
@@ -8,17 +8,17 @@ class Doge:
         self.contrib_name = 'dogebuild'
         self.pip = PipAdapter()
 
-    def use_plugin(self, plugin_name):
+    def use_plugin(self, plugin_name, **kwargs):
         plugin_full_name = self.contrib_name + '-' + plugin_name
         plugin_package = self.contrib_name + '_' + plugin_name
 
-        if not self.check_plugin_installed(plugin_package):
+        if not self._check_plugin_installed(plugin_package):
             self.pip.install(plugin_full_name)  # ???
 
-        file, path, desc = self.find_dotted_module(plugin_package + '.loader')
+        file, path, desc = self._find_dotted_module(plugin_package + '.loader')
         loader_file = imp.load_module('loader', file, path, desc)
 
-        plugin = loader_file.get()
+        plugin = loader_file.get(**kwargs)
         self.plugins.append(plugin)
         return plugin
 
@@ -30,16 +30,16 @@ class Doge:
         for t in tasks:
             t.run()
 
-    def find_dotted_module(self, module_name, path=None):
+    def _find_dotted_module(self, module_name, path=None):
         for x in module_name.split('.'):
             if path is not None:
                 path = [path]
             file, path, description = imp.find_module(x, path)
         return file, path, description
 
-    def check_plugin_installed(self, plugin_full_name):
+    def _check_plugin_installed(self, plugin_full_name):
         try:
-            self.find_dotted_module(plugin_full_name)
+            self._find_dotted_module(plugin_full_name)
             return True
         except ImportError:
             return False
