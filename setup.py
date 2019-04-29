cc9f4ce34ad355bb921b5355877068e9d0f97288
Kirill Sulim <kirillsulim@gmail.com>
2019-04-29 17:02:51 +0500

    Up version and fix broken distribution

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from os import path
 
 
 project_directory = path.abspath(path.dirname(__file__))
-data_files = []
 
 
 def load_from(file_name):
-    data_files.append(file_name)
     with open(path.join(project_directory, file_name), encoding='utf-8') as f:
         return f.read()
 
 
 setup(
     name='dogebuild',
-    version=load_from('dogebuild.version').strip(),
+    version=load_from('dogebuild/dogebuild.version').strip(),
     description='Builder with plugin system',
+    long_description=load_from('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/dogebuild/dogebuild',
     author='Kirill Sulim',
     author_email='kirillsulim@gmail.com',
@@ -23,6 +23,11 @@ setup(
     packages=find_packages(include=[
         'dogebuild',
     ]),
+    package_data={
+        'dogebuild': [
+            'dogebuild.version',
+        ]
+    },
     scripts=[
         'scripts/doge_runner.py',
     ],
