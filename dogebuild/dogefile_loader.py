a260020f10b4d993635e579c8b130e754c49f7aa
Kirill Sulim <kirillsulim@gmail.com>
2020-01-05 01:55:24 +0500

    Add cirectoy switch on loading

--- dogebuild/dogefile_loader.py
+++ dogebuild/dogefile_loader.py
@@ -1,11 +1,18 @@
+import os
+from pathlib import Path
+
 from dogebuild.plugins import ContextHolder
 from dogebuild.common import DOGE_FILE
 
 
 def load_doge_file(filename):
+    dir = Path(filename).resolve().parent
     with open(filename) as f:
         code = compile(f.read(), DOGE_FILE, 'exec')
 
         ContextHolder.create()
+        cwd = os.getcwd()
+        os.chdir(dir)
         exec(code)
+        os.chdir(cwd)
         return ContextHolder.clear_and_get()
